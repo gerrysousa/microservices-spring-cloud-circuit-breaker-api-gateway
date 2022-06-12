b66dc769901ad3b50ac6952c815b804ec5d866b9
Gerry Sousa <gerry@ciandt.com>
2022-06-12 10:35:10 -0300

    Importação do novo serviço

--- init/01.sql
+++ init/01.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `fornecedor`;
 CREATE DATABASE IF NOT EXISTS `loja`;
+CREATE DATABASE IF NOT EXISTS `transportador`;
 
 GRANT ALL ON `fornecedor`.* TO 'user'@'%';
 GRANT ALL ON `loja`.* TO 'user'@'%';
+GRANT ALL ON `transportador`.* TO 'user'@'%';
